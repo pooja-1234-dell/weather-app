ab3c606a25522208e4c19ca80a25775a4f41c386
pooja-1234-dell <muvvalapoojasri@gmail.com>
2026-01-04 17:45:43 +0530

    Update WeatherApplication.js

--- WeatherApplication.js
+++ WeatherApplication.js
@@ -27,27 +27,27 @@ searchIcon.addEventListener("click", async () => {
         let img = document.getElementById("weather_image");
 
         if (condition >= 200 && condition <= 232) {
-            img.src = "../ASSETS/img/WeatherThunder.png";
+            img.src = "/ASSETS/img/WeatherThunder.png";
         }
         else if (condition >= 300 && condition <= 321){
-             img.src = "../ASSETS/img/WeatherDrizzle.png";
+             img.src = "/ASSETS/img/WeatherDrizzle.png";
         }
         else if (condition >= 500 && condition <= 531){
-             img.src = "../ASSETS/img/WeatherRainy.png";
+             img.src = "/ASSETS/img/WeatherRainy.png";
         }
         else if (condition >= 600 && condition <= 622){
-             img.src = "../ASSETS/img/WeatherSnow.png";
+             img.src = "/ASSETS/img/WeatherSnow.png";
         }
         else if (condition >= 701 && condition <= 781) {
-            img.src = "../ASSETS/img/WeatherAtmosphere.png";
+            img.src = "/ASSETS/img/WeatherAtmosphere.png";
         }
         else if (condition === 800){
-             img.src = "../ASSETS/img/WeatherClearSky.png";
+             img.src = "/ASSETS/img/WeatherClearSky.png";
         }
         else if (condition >= 801 && condition <= 804){
-             img.src = "../ASSETS/img/WeatherCloudy.png";
+             img.src = "/ASSETS/img/WeatherCloudy.png";
         }
-        else img.src = "../ASSETS/img/WeatherDefault.png";
+        else img.src = "/ASSETS/img/WeatherDefault.png";
 
     } catch (error) {
         alert("Error: " + error.message);
